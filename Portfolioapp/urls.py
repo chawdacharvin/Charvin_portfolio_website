27c84ae6c21dc926c392c6c84f01340564030881
chawdacharvin <chawdacharvinj@gmail.com>
2025-07-28 13:50:21 +0530

    Initial commit: Django portfolio project

---
+++ Portfolioapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+urlpatterns = [
+    path("",views.home,name="home"),
+    path("about/",views.about,name="about"),
+    path("projects/",views.projects,name="project"),
+    path("contact/",views.contact,name="contact"),
+    path("chatbot/",views.chatbot_view,name="chatbot"),
+     path("healthcheck/",views.healthcheck,name="healthcheck"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
